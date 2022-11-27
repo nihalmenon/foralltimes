f309d03f963f3c9604224b1a1bd1332f56470c2a
abhinav-bala <abhinav.bala24@gmail.com>
2022-11-26 21:18:41 -0500

    init frontend and added object detection code

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="px-24 md:container md:mx-auto text-center">
+      <h1 className="py-24 text-4xl font-bold">
+       For All Times, There Exists A Space
+      </h1>
+    </div>
+    
+  );
+}
+
+export default App;
